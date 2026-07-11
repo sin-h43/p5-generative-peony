2bb9710fc70946fc394dfefdcf9e26b136e05900
Sinchana K <sinchanakrishnamurthy07@gmail.com>
2026-07-11 14:51:13 +0530

    Implement circle growth and shrinking animation in p5.js sketch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 // logical file. p5.js relies on two foundation function: setup() runs once and draw() runs on loop.
 
 const sketch = function(p) {
-    let circleX =0;
+    let circleSize =0;
+    let phase = 'growing';
+    let waitTimer = 0;
 
     p.setup = function(){
         p.createCanvas(600,400);
@@ -11,12 +13,32 @@ const sketch = function(p) {
         p.background(0);
         p.fill(255,100,150);
         p.noStroke();
-        p.circle(circleX, 200, 50);
-        circleX += 2;
 
-        if (circleX > p.width +25){
-            circleX = -25;
-        };
+        p.circle(p.width / 2, p.height / 2, circleSize);
+        
+        if(phase === 'growing'){
+            circleSize += 2;
+            //condition of transition for next phase
+            if(circleSize >= 200){
+                phase = 'waiting';
+                waitTimer = 0;
+            }
+        }
+        else if (phase === 'waiting'){
+            waitTimer++;
+            //~60 framer per second, 60frames = 1 sec
+            if(waitTimer >=60){
+                phase = 'shrinking';
+            }            
+        }
+        else if (phase === 'shrinking') {
+        circleSize = circleSize - 2;
+        
+        // Condition to loop back to the beginning
+            if (circleSize <= 0) {
+                phase = 'growing';
+            }
+        }
     };
 };
 
