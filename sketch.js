dfea2972f459f71cc50c8c454ecedfabc25c7049
Sinchana K <sinchanakrishnamurthy07@gmail.com>
2026-07-11 14:37:36 +0530

    Refactor p5.js sketch structure and update HTML for container integration

--- sketch.js
+++ sketch.js
@@ -1,28 +1,23 @@
 // logical file. p5.js relies on two foundation function: setup() runs once and draw() runs on loop.
 
-function setup() {
-    //creates a drawing canvas 600 pixels wide and 400 pixels tall
-    createCanvas(600,400);
-}    
+const sketch = function(p) {
+    let circleX =0;
 
-function draw(){
-    //redraw the bg every frame to erase the prev circpe
-    background(0);
+    p.setup = function(){
+        p.createCanvas(600,400);
+    };
 
-    //sets the inside color of the shapes drawn after it (RGB)
-    fill(255,100,150);
+    p.draw = function(){
+        p.background(0);
+        p.fill(255,100,150);
+        p.noStroke();
+        p.circle(circleX, 200, 50);
+        circleX += 2;
 
-    //removes the outline of the shape drawn after it
-    noStroke();
+        if (circleX > p.width +25){
+            circleX = -25;
+        };
+    };
+};
 
-    //draws a circle at (x,y,diameter)
-    circle(circleX,200,50);
-
-    //updates the position for the next frame to create movement
-    circleX += 2;
-
-    //resets the posi if it goes off the right edge
-    if(circleX > width+25){
-        circleX = -25;
-    }
-}
+new p5(sketch, document.getElementById('p5-container'));
